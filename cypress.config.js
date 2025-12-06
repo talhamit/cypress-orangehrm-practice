3498162c8917d54a89ba056c11f800ba425f51ac
Talha <talhamit@gmail.com>
2025-12-07 00:55:17 +0500

    Code update

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php",  // Base URL added
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/login/loginvalidity.cy.js
@@ -0,0 +1,93 @@
+describe('Login Validity', () =>{
+
+    //beforeEach(() => {
+        // cy.session('Orange HRM login', ()=> {
+        // cy.visit('/auth/login');
+        // cy.get('input[placeholder="Username"]').type('Admin');
+        // cy.get('input[placeholder="Password"]').type('admin123');
+        // cy.get('button[type="submit"]').click();
+        // cy.contains("Dashboard").should('be.visible');
+
+        //})
+
+        it('Validate Empty Field', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('button[type="submit"]').click();
+         cy.get('input[name="username"]').closest('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+         cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+        })
+
+         it('Valid username Empty password', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('input[placeholder="Username"]').type('Admin');
+         cy.get('button[type="submit"]').click();
+         cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+        })
+
+        it('Invalid username Empty password', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('input[placeholder="Username"]').type('dmin');
+         //cy.get('input[placeholder="Password"]').type('');
+         cy.get('button[type="submit"]').click();
+         //cy.get('input[name="username"]').closest('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+         cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+        })
+
+        it('Invalid username valid password', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('input[placeholder="Username"]').type('dmin');
+         cy.get('input[placeholder="Password"]').type('admin123');
+         cy.get('button[type="submit"]').click();
+         cy.get('.oxd-alert-content-text').should('have.text','Invalid credentials')
+         
+        })
+
+        it('Valid username invalid password', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('input[placeholder="Username"]').type('admin');
+         cy.get('input[placeholder="Password"]').type('admin1');
+         cy.get('button[type="submit"]').click();
+         cy.get('.oxd-alert-content-text').should('have.text','Invalid credentials')
+         
+        })
+
+        it('inValid username invalid password', ()=> {
+
+         cy.visit('auth/login');
+         cy.get('input[placeholder="Username"]').type('aadmin');
+         cy.get('input[placeholder="Password"]').type('admin1');
+         cy.get('button[type="submit"]').click();
+         cy.get('.oxd-alert-content-text').should('have.text','Invalid credentials')
+         
+        })
+
+        it('Empty username valid password', ()=> {
+
+         cy.visit('auth/login');
+         //cy.get('input[placeholder="Username"]').type('Admin');
+         cy.get('input[placeholder="Password"]').type('admin123');
+         cy.get('button[type="submit"]').click();
+         cy.get('input[name="username"]').closest('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+         //cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+        })
+
+        it('Empty username invalid password', ()=> {
+
+         cy.visit('auth/login');
+         //cy.get('input[placeholder="Username"]').type('Admin');
+         cy.get('input[placeholder="Password"]').type('11111');
+         cy.get('button[type="submit"]').click();
+         cy.get('input[name="username"]').closest('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+         //cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
+        })
+
+
+    })
+    
+
+
