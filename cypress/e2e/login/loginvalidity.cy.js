f7a306aa3c99f3dbf572e73cf61aef0ee42ba72b
Talha <talhamit@gmail.com>
2025-12-07 18:27:13 +0500

    Code update

--- cypress/e2e/login/loginvalidity.cy.js
+++ cypress/e2e/login/loginvalidity.cy.js
@@ -86,6 +86,33 @@ describe('Login Validity', () =>{
          //cy.get('input[name="password"]').parents('.oxd-input-group').find('.oxd-input-field-error-message').should('have.text', 'Required');
         })
 
+        it('Forget Password', () => {
+            cy.visit('auth/login');
+            cy.get('.orangehrm-login-forgot').should('be.visible').and('have.text','Forgot your password? ').click()
+            cy.url().should('include','/auth/requestPasswordResetCode')
+        })
+
+
+        
+        it('Verify the social icons', () =>{
+            const social_icon = ["linkedin","facebook","twitter.com","youtube"]
+            var i=0;
+            social_icon.forEach(item =>{
+               
+                cy.visit('/auth/login');
+                cy.get('.orangehrm-login-footer-sm a').eq(i).invoke('attr','href').should('contain',item);
+                cy.get('.orangehrm-login-footer-sm a').eq(i).click()
+                i++;
+                //cy.url().should('contain',item)
+            })
+         })
+
+         it.only('Check Footer orangeHrm link', () =>{
+
+            cy.visit('/auth/login')
+            cy.get('p.orangehrm-copyright a').click()
+        })
+
 
     })
     
